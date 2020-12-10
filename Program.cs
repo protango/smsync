745b93e8ff0c1f81932e95a5c0a8db985f987799
Zac Murray <zac.m@timetabling.com.au>
2020-12-10 14:30:23 +1000

    Add .NET Core project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.CommandLine;
+
+namespace smsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RootCommand rootCommand = new RootCommand(
+              description: "Converts an image file from one format to another."
+              , treatUnmatchedTokensAsErrors: true);
+        }
+    }
+}
